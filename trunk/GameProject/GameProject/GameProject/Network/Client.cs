d2b74eb86a6e02454297cd89d48d9ecf62f30922
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@f15e883c-e23b-6ebe-b638-94be3b9f1045>
2013-07-03 13:20:37 +0000

    Update more core

---
+++ trunk/GameProject/GameProject/GameProject/Network/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XnaGameNetworkEngine;
+using Microsoft.Xna.Framework;
+
+namespace GameProject.Network
+{
+    public class Client:AsyncTcpClient
+    {
+        public Client(Game game)
+            : base(game)
+        {
+        }
+
+        protected override void OnDataReceived(string data, SocketPacket packet)
+        {
+            base.OnDataReceived(data, packet);
+        }
+
+        protected override void OnConnected(SocketPacket server)
+        {
+            base.OnConnected(server);
+        }
+    }
+}
