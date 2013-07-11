a35697ce803c905df32a2be5638d9195a8f15956
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@f15e883c-e23b-6ebe-b638-94be3b9f1045>
2013-07-11 16:13:56 +0000

    Update GameProject.csproj

---
+++ trunk/GameProject/GameProject/GameProject/Network/RequestHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameProject.Network
+{
+    public static class RequestHandler
+    {
+        public static void SendCreateGame(Client clt)
+        {
+
+        }
+    }
+}
