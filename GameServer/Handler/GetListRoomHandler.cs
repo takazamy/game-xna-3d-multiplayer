59c92e27d4223d0728d9c965fe9176ea5c629101
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@f15e883c-e23b-6ebe-b638-94be3b9f1045>
2013-07-21 02:10:49 +0000

    Update Handler server

---
+++ GameServer/Handler/GetListRoomHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+namespace GameServer.Handler
+{
+    public class GetListRoomHandler:IHandler
+    {
+
+        public GetListRoomHandler(Client clt)
+            :base(clt)
+        {
+
+        }
+
+        public override void Handler(JObject data)
+        {
+            JObject room;
+            JArray roomList = new JArray();
+            foreach (var item in ServerManager.roomList)
+            {
+                room = new JObject();
+                room.Add(GameKeys.ROOMID, item.Key);
+                room.Add(GameKeys.NUMBER_USERS, item.Value.clientList.Count);
+                room.Add(GameKeys.MAX, item.Value.maxPlayer);
+                roomList.Add(room);
+            }
+            JObject mesg = new JObject();
+            mesg[GameCommand.COMMAND] = GameCommand.GET_LIST_ROOM;
+            mesg[GameKeys.ROOM_LIST] = roomList;
+            this.client.send(mesg.ToString());
+        }
+
+        public override bool Valid(JObject data)
+        {
+            //Kiểm tra dữ liệu hợp lệ trước khi vào Handler
+            return true;
+        }
+    }
+}
