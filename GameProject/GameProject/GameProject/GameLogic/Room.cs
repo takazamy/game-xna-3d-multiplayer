b122a08edc5bd75eefa477c6092165270246ee2e
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@f15e883c-e23b-6ebe-b638-94be3b9f1045>
2013-07-21 07:21:39 +0000

    Add Room CLient

---
+++ GameProject/GameProject/GameProject/GameLogic/Room.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameProject.Network;
+using Microsoft.Xna.Framework;
+
+namespace GameProject.GameLogic
+{
+    public class Room
+    {
+        public Dictionary<int, Participant> clientList;
+        Client clt;
+        public int totalPlayer = 0;
+        public Room()
+        {
+            totalPlayer = 0;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            foreach (var item in clientList)
+            {
+                item.Value.Update(gameTime);
+            }
+        }
+
+        public void Add(Participant p)
+        {
+            clientList.Add(p.ClientId, p);
+            totalPlayer++;
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            foreach (var item in clientList)
+            {
+                item.Value.Draw(gameTime);
+            }
+
+            
+        }
+    }
+}
